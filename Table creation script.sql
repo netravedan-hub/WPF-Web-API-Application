033caddb357f39069ba6d6457391f5491df20bba
netravedan-hub <78358728+netravedan-hub@users.noreply.github.com>
2021-02-02 10:12:36 +0530

    Add files via upload

---
+++ Table creation script.sql
@@ -0,0 +1,13 @@
+
+
+create table ContactDetails
+(
+Id int not null,
+FirstName varchar(255),
+LastName varchar(255),
+PhoneNumber varchar(255),
+Email varchar(255),
+CurrentStatus varchar(255),
+Primary key(Id)
+)
+
